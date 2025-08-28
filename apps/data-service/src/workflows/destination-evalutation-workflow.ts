85bc2d9f0f55b6eb3ccb9371527f459452f0f516
Tomas Rokos <tomas@rokos.cz>
2025-08-28 11:16:41 +0200

    Add evaluations for the links

--- apps/data-service/src/workflows/destination-evalutation-workflow.ts
+++ apps/data-service/src/workflows/destination-evalutation-workflow.ts
@@ -1,14 +1,48 @@
+import { aiDestinationChecker } from '@/helpers/ai-destination-checker';
+import { collectDestinationInfo } from '@/helpers/browser-render';
 import { WorkflowEntrypoint, WorkflowEvent, WorkflowStep } from 'cloudflare:workers';
+import { addEvaluation } from '@repo/data-ops/queries/evaluations';
+import { initDatabase } from '@repo/data-ops/database';
 
-export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, unknown> {
-	async run(event: Readonly<WorkflowEvent<unknown>>, step: WorkflowStep) {
+export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, DestinationStatusEvaluationParams> {
+	async run(event: Readonly<WorkflowEvent<DestinationStatusEvaluationParams>>, step: WorkflowStep) {
+		initDatabase(this.env.DB);
+
 		const collectedData = await step.do('Collect rendered destination page data', async () => {
-			console.log('Collecting rendered destination page data');
-			throw new Error('test');
-			return {
-				dummydata: 'dummydata',
-			};
+			return collectDestinationInfo(this.env, event.payload.destinationUrl);
 		});
+
+		const aiStatus = await step.do(
+			'Use AI to check status of page',
+			{
+				retries: {
+					limit: 0,
+					delay: 0,
+				},
+			},
+			async () => {
+				return await aiDestinationChecker(this.env, collectedData.bodyText);
+			}
+		);
+
+		const evaluationId = await step.do('Save evaluation in database', async () => {
+			return await addEvaluation({
+				linkId: event.payload.linkId,
+				status: aiStatus.status,
+				reason: aiStatus.statusReason,
+				accountId: event.payload.accountId,
+				destinationUrl: event.payload.destinationUrl,
+			});
+		});
+
+		await step.do('Backup destination HTML in R2', async () => {
+			const accountId = event.payload.accountId;
+			const r2PathHtml = `evaluations/${accountId}/html/${evaluationId}`;
+			const r2PathBodyText = `evaluations/${accountId}/body-text/${evaluationId}`;
+			await this.env.BUCKET.put(r2PathHtml, collectedData.html);
+			await this.env.BUCKET.put(r2PathBodyText, collectedData.bodyText);
+		});
+
 		console.log(collectedData);
 	}
 }
